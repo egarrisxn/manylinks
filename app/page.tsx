d8dda6d08bfd4af4cac56e815c029891836106f4
egarrisxn <egarrisxn@gmail.com>
2025-05-09 03:13:07 -0400

    done for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import GoogleLogin from "@/components/google-login";
 import GitHubLogin from "@/components/github-login";
 import ImageCarousel from "@/components/image-carousel";
@@ -27,9 +28,14 @@ export default function LandingPage() {
           <div className='w-fit'>
             <div className='flex flex-col items-center text-center md:items-start md:text-start'>
               <h1 className='w-full max-w-md text-6xl font-bold md:text-7xl xl:max-w-xl xl:text-8xl'>
-                One Page. Many Links.
+                One Page.
+                <span className='to-primary bg-gradient-to-r from-[#D247BF] bg-clip-text px-2 text-transparent'>
+                  {" "}
+                  Many{" "}
+                </span>
+                Links.
               </h1>
-              <h2 className='my-6 w-full max-w-md text-xl text-gray-500 md:text-2xl xl:max-w-xl xl:text-3xl'>
+              <h2 className='text-accent-foreground/70 my-6 w-full max-w-md text-xl md:text-2xl xl:max-w-xl xl:text-3xl'>
                 Help your followers discover everything you do, with one simple
                 link.
               </h2>
@@ -39,7 +45,8 @@ export default function LandingPage() {
               </div>
             </div>
           </div>
-          <div className='w-fit'>
+          <div className='group relative w-fit'>
+            <div className='bg-primary/50 absolute top-2 left-1/2 mx-auto h-24 w-[90%] -translate-x-1/2 transform rounded-full blur-3xl lg:-top-8 lg:h-80'></div>
             <ImageCarousel />
           </div>
         </section>
@@ -47,7 +54,7 @@ export default function LandingPage() {
           <h1 className='mb-4 text-5xl font-bold'>
             Your all-in-one link hub, built for creators like you
           </h1>
-          <p>
+          <p className='text-muted-foreground'>
             Share more than just a link. Combine your profiles, content, store,
             and projects in one stylish, customizable page. No coding required!
           </p>
@@ -59,17 +66,37 @@ export default function LandingPage() {
           <h1 className='mb-4 text-5xl font-bold'>
             Make your first impression unforgettable
           </h1>
-          <p>
+          <p className='text-muted-foreground'>
             Whether you are a creator, entrepreneur, or brand, connect your
             audience to everything you offer with a sleek micro-site that iss
             always up to date.
           </p>
         </section>
-        <section className='mt-4 mb-12'>
+        <section className='group relative mt-4 mb-12'>
           <ImageGrid />
+          <div className='from-background/0 via-background/50 to-background absolute bottom-0 left-0 h-20 w-full rounded-lg bg-gradient-to-b md:h-28'></div>
+        </section>
+        <section className='mx-auto mt-20 flex w-full max-w-lg flex-col items-center justify-center text-center'>
+          <h1 className='mb-4 text-5xl font-bold'>
+            Totally Free and 100%
+            <span className='to-primary bg-gradient-to-r from-[#D247BF] bg-clip-text pl-2 text-transparent'>
+              Open Source
+            </span>
+          </h1>
+          <p className='text-muted-foreground w-full max-w-md'>
+            Blah blah blah filler words need to be here yadda yadda bam! Here we
+            gooooo! Singing and dancing and yeah.
+          </p>
+          <div className='mt-4 mb-20'>
+            <Button asChild>
+              <Link href='/create' target='_blank'>
+                Lets begin!
+              </Link>
+            </Button>
+          </div>
         </section>
       </div>
-      <footer className='mt-8 py-4'>
+      <footer className='py-4'>
         <div className='mx-auto flex max-w-6xl items-center justify-between px-4'>
           <section className='flex flex-row items-center gap-1'>
             <Image
@@ -82,7 +109,7 @@ export default function LandingPage() {
             <span className='hidden font-bold lg:block'>ManyLinks</span>
           </section>
           <section>
-            <p className='text-center text-sm tracking-tight text-zinc-700'>
+            <p className='text-muted-foreground text-center text-sm tracking-tight'>
               © {new Date().getFullYear()} ManyLinks. All Rights Reserved.
             </p>
           </section>
@@ -92,7 +119,7 @@ export default function LandingPage() {
               href='https://github.com/egarrisxn/manylinks'
               target='_blank'
               rel='noopener noreferrer'
-              className='text-zinc-700 hover:text-zinc-900'
+              className='text-muted-foreground hover:text-accent-foreground'
             >
               <GitHubIcon />
             </Link>
--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -4,13 +4,15 @@ interface ImageGridProps {
 
 function ImageGridCard({ images }: ImageGridProps) {
   return (
-    <div className='grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-4'>
+    <div className='grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-4 lg:gap-0'>
       {images.map((image, index) => (
         <div key={index} className='flex items-center justify-center'>
           <img
             src={image}
             alt={`image-${index}`}
-            className='h-auto w-full rounded-[2.2rem] transition-transform duration-300 hover:scale-105'
+            className={`size-full rounded-[2.2rem] transition-transform duration-300 hover:scale-105 hover:rotate-0 ${
+              index % 2 === 0 ? "-rotate-5" : "rotate-5"
+            }`}
           />
         </div>
       ))}
